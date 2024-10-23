dd5b9eefd936baecea259fb5777c553f811ff4c5
habanagi <tnbnagi@gmail.com>
2024-10-23 20:45:58 +0900

    ko->en,en->jaと翻訳する事で精度向上

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-import {Recognition} from './speechRecognize';
-import {translateText} from './googleTranslate'
+import { Recognition } from "./speechRecognize";
+import { translateText } from "./googleTranslate";
 
 Recognition((text: string) => {
-    translateText(text + ".", 'ko', 'ja')
-        .then(result => {
-            console.log(result.text)
-            console.log(`%c${text}`, 'font-size: 5px;');
-        })
-        .catch(error => console.error(error));
+  translateText(text + ".", "ko", "en")
+    .then((resultEn) => {
+      return translateText(resultEn.text, "en", "ja");
+    })
+    .then((resultJa) => {
+      console.log(resultJa.text);
+      console.log(`%c${text}`, "font-size: 5px;");
+    })
+    .catch((error) => console.error(error));
 });
--- src/speechRecognize.ts
+++ src/speechRecognize.ts
@@ -1,35 +1,42 @@
-
 let timeout: any = null;
 
-const recognition = new ((window as any).SpeechRecognition || (window as any).webkitSpeechRecognition)();
-recognition.lang = 'ko-KR';
+const recognition = new ((window as any).SpeechRecognition ||
+  (window as any).webkitSpeechRecognition)();
+recognition.lang = "ko-KR";
 recognition.interimResults = true;
 recognition.continuous = true;
 
 let lastRecognizedWordCount = 0;
+document.addEventListener("keydown", (event) => {
+  if (event.key === "z") {
+    recognition.stop();
+  }
+});
 
 const Recognition = (resultsCallBack: (text: string) => void) => {
-    recognition.onresult = (event: any) => {
-        for (let i = event.resultIndex; i < event.results.length; ++i) {
-            const text =  event.results[i][0].transcript;
-            const recognizedWordCount : number = text.split(" ").length
-            if (event.results[i].isFinal) {
-                resultsCallBack(text);
-                lastRecognizedWordCount = 0
-                recognition.stop();
-            }else{
-                if(recognizedWordCount >= lastRecognizedWordCount + 5){
-                    resultsCallBack(text);
-                    lastRecognizedWordCount = recognizedWordCount;
-                }
-            }
+  recognition.onresult = (event: any) => {
+    for (let i = event.resultIndex; i < event.results.length; ++i) {
+      const text = event.results[i][0].transcript;
+      const recognizedWordCount: number = text.split(" ").length;
+      if (event.results[i].isFinal) {
+        resultsCallBack(text);
+        lastRecognizedWordCount = 0;
+        recognition.stop();
+      } else {
+        if (text.includes("니다")) {
+          recognition.stop();
+        } else if (recognizedWordCount >= lastRecognizedWordCount + 5) {
+          resultsCallBack(text);
+          lastRecognizedWordCount = recognizedWordCount;
         }
-    };
+      }
+    }
+  };
 
-    recognition.onend = () => {
-        Recognition(resultsCallBack);
-    };
+  recognition.onend = () => {
+    Recognition(resultsCallBack);
+  };
 
-    recognition.start();
-}
-export {Recognition};
+  recognition.start();
+};
+export { Recognition };
